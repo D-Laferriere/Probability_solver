08d2268b7701aafe38ce662da332625f468c1a91
dll3182 <dll3182@rit.edu>
2023-02-25 18:52:15 -0500

    Began working on a proper commandline interface

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,19 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args){
-        ArrayList<Double> p = new ArrayList<>();
-        p.add(0.3);
-        p.add(0.5);
-        p.add(0.1);
-        p.add(0.1);
-        ArrayList<Double> o = new ArrayList<>();
-        o.add(1.0);
-        o.add(2.0);
-        o.add(3.0);
-        o.add(4.0);
-        Expectation e = new Expectation(p,o);
-        Variance v = new Variance(p,o);
-        System.out.println(e.getExpectation()+ "\n" + v.getVariance());
+        introduction();
+    }
+
+
+
+    private static void introduction(){
+        System.out.println("Welcome to Probability Solver!\n"
+                + "==============================\n"
+                + "Commands:\nHelp - lists commands");
+        help();
+    }
+    private static void help(){
+        System.out.println("Problems - lists all currently implemented equations\n"
+                + "Quit - exits the program");
     }
 }
--- src/problems/Binomial_Distribution.java
+++ src/problems/Binomial_Distribution.java
@@ -1,4 +1,22 @@
 package problems;
 
-public class Binomial_Distribution {
+public class Binomial_Distribution implements Problem{
+
+
+
+
+
+
+
+
+
+    @Override
+    public void example() {
+        System.out.println("Binomial Distribution:\n" +
+                "1. Binary outcome\n" +
+                "2. P(Success) is constant across trials\n" +
+                "3. Trials have to be independent \n" +
+                "4. More than one trial, n trials\n" +
+                "X = number of successes in n trials");
+    }
 }
